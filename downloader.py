fbf470a06e491091710046b23aaa34754730038c
ChNokia <chizarus@mail.ru>
2014-08-21 13:30:33 +0300

    Update downloader.py
    Add function read_url_page, get_links_list, download_file

--- downloader.py
+++ downloader.py
@@ -1,5 +1,41 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import os
+import traceback
+
+def read_url_page(url):
+    #page = urllib.request.urlopen(url) paste when url - http instead with open
+    with open(url, 'r') as page:
+        page = page.read()
+    
+    return page
+    
+def get_links_list(url):
+    links_list = []
+    page = read_url_page(url)
+    soup = BeautifulSoup(page)
+    
+    href_list = soup.pre.hr.findAll('a')
+    
+    for link in href_list:
+        links_list.append(link.get('href'))
+    
+    return links_list
+
+def download_file(url, file_name = None):
+    if file_name == None:
+        file_name = urllib.parse.urlparse(url)
+    
+    file_name = os.path.basename(file_name.path)
+    
+    try:
+        urllib.request.urlretrieve(url, file_name)
+        
+        return True
+    except:
+        traceback.print_exc()
+        
+        return False
 
 def main():
     text_html = ''
@@ -7,16 +43,13 @@ def main():
     
     #page = urllib.request.urlopen('saved_resource.html')
     
-    with open('saved_resource.html', 'r') as file:
-        text_html = file.read()
-        
-    soup = BeautifulSoup(text_html)
+    folders_list = get_links_list('saved_resource.html')
     
-    folders_list = soup.pre.hr.findAll('a')
     
     for link in folders_list:
-        print(link.get('href'))
+        print(link)
 
 if __name__ == '__main__':
     main()
+    
     
